e66819120c3b85b2ed57c47a08adefe25ef1ad11
Nathan Smith <nathan@nathan-smith.org>
2022-05-07 22:37:05 -0400

    feat: add chip component

---
+++ src/components/chip/chip.stories.ts
@@ -0,0 +1,9 @@
+import "./chip.scss";
+import chipColors from "./chip-colors.html";
+
+export default {
+  title: "Components/Chip",
+  parameters: { layout: "centered" },
+};
+
+export const colors = () => chipColors;
